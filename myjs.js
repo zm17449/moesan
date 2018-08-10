d9ffd0c1b7d08952588626df0ed53869f0463bd1
zm17449 <zm17449@josai.ac.jp>
2018-08-10 05:40:17 -0400

    message

--- myjs.js
+++ myjs.js
@@ -9,3 +9,35 @@
 //alert(Number('2')+Number('3'));
 //alert('2'+3);
 //alert(Number('2')+3);
+
+
+function showimg1(){
+  document.area1.src = "disny.jpg";
+}
+
+function showimg2(){
+  document.area1.src = "eno.jpg";
+}
+
+function showimg3(){
+  document.area1.src = "sirasu.jpg";
+}
+
+function showimg4(){
+  document.area1.src = "tako.jpg";
+}
+function showimg5(){
+  document.area1.src = "aisu.jpg";
+}
+function showimg6(){
+  document.area1.src = "4.JPG";
+}
+function showimg7(){
+  document.area1.src = "3.JPG";
+}
+function showimg8(){
+  document.area1.src = "2.5.JPG";
+}
+function showimg9(){
+  document.area1.src = "1.5.JPG";
+}
